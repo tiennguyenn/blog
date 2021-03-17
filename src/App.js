a4cd427a955f1d3695bab09c0e0c3929bf8364d1
Tien Nguyen <tienn6981@gmail.com>
2021-03-17 07:17:52 +0700

    add home page.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,185 @@
-import logo from './logo.svg';
-import './App.css';
+import homeBg from './assets/images/home-bg.jpg'
+import $ from 'jquery'
+
+import './assets/css/clean-blog.min.css'
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    // Show the navbar when the page is scrolled up
+  const MQL = 992;
+
+  //primary navigation slide-in effect
+  if ($(window).width() > MQL) {
+    const headerHeight = $('#mainNav').height();
+    $(window).on('scroll', {
+        previousTop: 0
+      },
+      function() {
+        const currentTop = $(window).scrollTop();
+        //check if user is scrolling up
+        if (currentTop < this.previousTop) {
+          //if scrolling up...
+          if (currentTop > 0 && $('#mainNav').hasClass('is-fixed')) {
+            $('#mainNav').addClass('is-visible');
+          } else {
+            $('#mainNav').removeClass('is-visible is-fixed');
+          }
+        } else if (currentTop > this.previousTop) {
+          //if scrolling down...
+          $('#mainNav').removeClass('is-visible');
+          if (currentTop > headerHeight && !$('#mainNav').hasClass('is-fixed')) $('#mainNav').addClass('is-fixed');
+        }
+        this.previousTop = currentTop;
+      });
+  }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      {/* Navigation */}
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+        <div className="container">
+          <a className="navbar-brand" href="index.html">Start Bootstrap</a>
+          <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            Menu
+            <i className="fas fa-bars" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarResponsive">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="index.html">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="about.html">About</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="post.html">Sample Post</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="contact.html">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      {/* Page Header */}
+      <header className="masthead" style={{backgroundImage: `url(${homeBg})`}}>
+        <div className="overlay" />
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-8 col-md-10 mx-auto">
+              <div className="site-heading">
+                <h1>Clean Blog</h1>
+                <span className="subheading">A Blog Theme by Start Bootstrap</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </header>
-    </div>
+      {/* Main Content */}
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-8 col-md-10 mx-auto">
+            <div className="post-preview">
+              <a href="post.html">
+                <h2 className="post-title">
+                  Man must explore, and this is exploration at its greatest
+                </h2>
+                <h3 className="post-subtitle">
+                  Problems look mighty small from 150 miles up
+                </h3>
+              </a>
+              <p className="post-meta">Posted by
+                <a href="#">Start Bootstrap</a>
+                on September 24, 2019</p>
+            </div>
+            <hr />
+            <div className="post-preview">
+              <a href="post.html">
+                <h2 className="post-title">
+                  I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.
+                </h2>
+              </a>
+              <p className="post-meta">Posted by
+                <a href="#">Start Bootstrap</a>
+                on September 18, 2019</p>
+            </div>
+            <hr />
+            <div className="post-preview">
+              <a href="post.html">
+                <h2 className="post-title">
+                  Science has not yet mastered prophecy
+                </h2>
+                <h3 className="post-subtitle">
+                  We predict too much for the next year and yet far too little for the next ten.
+                </h3>
+              </a>
+              <p className="post-meta">Posted by
+                <a href="#">Start Bootstrap</a>
+                on August 24, 2019</p>
+            </div>
+            <hr />
+            <div className="post-preview">
+              <a href="post.html">
+                <h2 className="post-title">
+                  Failure is not an option
+                </h2>
+                <h3 className="post-subtitle">
+                  Many say exploration is part of our destiny, but it’s actually our duty to future generations.
+                </h3>
+              </a>
+              <p className="post-meta">Posted by
+                <a href="#">Start Bootstrap</a>
+                on July 8, 2019</p>
+            </div>
+            <hr />
+            {/* Pager */}
+            <div className="clearfix">
+              <a className="btn btn-primary float-right" href="#">Older Posts →</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr />
+      {/* Footer */}
+      <footer>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-8 col-md-10 mx-auto">
+              <ul className="list-inline text-center">
+                <li className="list-inline-item">
+                  <a href="#">
+                    <span className="fa-stack fa-lg">
+                      <i className="fas fa-circle fa-stack-2x" />
+                      <i className="fab fa-twitter fa-stack-1x fa-inverse" />
+                    </span>
+                  </a>
+                </li>
+                <li className="list-inline-item">
+                  <a href="#">
+                    <span className="fa-stack fa-lg">
+                      <i className="fas fa-circle fa-stack-2x" />
+                      <i className="fab fa-facebook-f fa-stack-1x fa-inverse" />
+                    </span>
+                  </a>
+                </li>
+                <li className="list-inline-item">
+                  <a href="#">
+                    <span className="fa-stack fa-lg">
+                      <i className="fas fa-circle fa-stack-2x" />
+                      <i className="fab fa-github fa-stack-1x fa-inverse" />
+                    </span>
+                  </a>
+                </li>
+              </ul>
+              <p className="copyright text-muted">Copyright © Your Website 2020</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
   );
 }
 
